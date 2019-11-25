a690a57210004f055c8b0b6949b61405eb650245
zhangpengcheng.zpc <zhangpengcheng.zpc@bytedance.com>
2019-11-25 20:25:39 +0800

    model

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('invite_page/<invitor_id>/', views.invite_page, name='invite_page'),
-    path('update_page/<uuid>/<key>/', views.update_page, name='update_page'),
+    path('update_page/<user_id>/<key>/', views.update_page, name='update_page'),
     path('new_subscribe/<invitor_id>/', views.new_subscribe, name='new_subscribe'),
-    path('update_subscribe/<uuid>/<key>/', views.update_subscribe, name='update_subscribe'),
-    path('pay/<uuid>/<int:month>/', views.pay, name='pay'),
+    path('update_subscribe/<user_id>/<key>/', views.update_subscribe, name='update_subscribe'),
+    path('pay/<user_id>/<int:month>/', views.pay, name='pay'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import get_object_or_404, render
 from .models import Subscribe, User, Magazine
 import uuid
 from django.utils import timezone
+from django.http import Http404
+
 
 def invite_page(request, invitor_id):
     user = get_object_or_404(User, uuid=invitor_id)
     return render(request, 'invite.html', {'user': user})
-
-
-def update_page(request, uuid, key):
-    return render(request, 'update_subscribe.html', {})
 
 
 def new_subscribe(request, invitor_id):
@@ -17,12 +15,21 @@ def new_subscribe(request, invitor_id):
     user = User(email=email, uuid=uuid.uuid4().hex, key=uuid.uuid4().hex, invitor=invitor_id,
                 expire_date=timezone.now())
     user.save()
-    return render(request, 'subscribe_ok.html', {})
+    return render(request, 'subscribe_ok.html', {'user': user})
 
 
-def update_subscribe(request, uuid, key):
-    return render(request, 'subscribe_ok.html', {})
+def update_page(request, user_id, key):
+    user = get_object_or_404(User, uuid=user_id, key=key)
+    return render(request, 'update_subscribe.html', {'user': user})
 
 
-def pay(request, uuid, month):
-    return render(request, 'pay.html', {})
+def update_subscribe(request, user_id, key):
+    user = get_object_or_404(User, uuid=user_id, key=key)
+    return render(request, 'subscribe_ok.html', {'user': user})
+
+
+def pay(request, user_id, month):
+    user = get_object_or_404(User, uuid=user_id)
+    if month not in [1, 3, 6, 12]:
+        raise Http404("Question does not exist")
+    return render(request, 'pay.html', {'user': user})
